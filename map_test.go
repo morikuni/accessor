630812e65fbefca419f32eed17a66de494c12e79
morikuni <taihei.with+git@gmail.com>
2017-02-15 22:28:17 +0900

    Add test of MapObject.Set

--- map_test.go
+++ map_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestMapObject(t *testing.T) {
+func TestMapObject_Get(t *testing.T) {
 	type Input struct {
 		Object Object
 		Path   string
@@ -101,6 +101,129 @@ func TestMapObject(t *testing.T) {
 			assert := assert.New(t)
 
 			obj, err := testCase.Input.Object.Get(testCase.Input.Path, testCase.Input.Paths...)
+
+			assert.Equal(testCase.Expect.Object, obj)
+			assert.Equal(testCase.Expect.Err, err)
+		})
+	}
+}
+
+func TestMapObject_Set(t *testing.T) {
+	type Input struct {
+		Object Object
+		Path   string
+		Paths  []string
+		BeSet  Object
+	}
+	type Expect struct {
+		Object Object
+		Err    error
+	}
+	type Test struct {
+		Title  string
+		Input  Input
+		Expect Expect
+	}
+
+	table := []Test{
+		Test{
+			Title: "success",
+			Input: Input{
+				Object: MapObject(map[string]Object{
+					"a": DummyObject{1},
+				}),
+				Path:  "a",
+				Paths: nil,
+				BeSet: DummyObject{2},
+			},
+			Expect: Expect{
+				Object: MapObject(map[string]Object{
+					"a": DummyObject{2},
+				}),
+				Err: nil,
+			},
+		},
+		Test{
+			Title: "success nested",
+			Input: Input{
+				Object: MapObject(map[string]Object{
+					"a": MapObject(map[string]Object{
+						"b": MapObject(map[string]Object{
+							"c": DummyObject{1},
+						}),
+					}),
+				}),
+				Path:  "a",
+				Paths: []string{"b"},
+				BeSet: DummyObject{2},
+			},
+			Expect: Expect{
+				Object: MapObject(map[string]Object{
+					"a": MapObject(map[string]Object{
+						"b": DummyObject{2},
+					}),
+				}),
+				Err: nil,
+			},
+		},
+		Test{
+			Title: "path error",
+			Input: Input{
+				Object: MapObject(map[string]Object{
+					"a": DummyObject{1},
+				}),
+				Path:  "x",
+				Paths: nil,
+				BeSet: DummyObject{2},
+			},
+			Expect: Expect{
+				Object: MapObject(map[string]Object{
+					"a": DummyObject{1},
+				}),
+				Err: &PathError{
+					Message: "no such path",
+					Path:    "x",
+					Stack:   nil,
+				},
+			},
+		},
+		Test{
+			Title: "path error nested",
+			Input: Input{
+				Object: MapObject(map[string]Object{
+					"a": MapObject(map[string]Object{
+						"b": MapObject(map[string]Object{
+							"c": DummyObject{1},
+						}),
+					}),
+				}),
+				Path:  "a",
+				Paths: []string{"b", "x"},
+				BeSet: DummyObject{2},
+			},
+			Expect: Expect{
+				Object: MapObject(map[string]Object{
+					"a": MapObject(map[string]Object{
+						"b": MapObject(map[string]Object{
+							"c": DummyObject{1},
+						}),
+					}),
+				}),
+				Err: &PathError{
+					Message: "no such path",
+					Path:    "x",
+					Stack:   []string{"b", "a"},
+				},
+			},
+		},
+	}
+
+	for _, testCase := range table {
+		t.Run(testCase.Title, func(t *testing.T) {
+			assert := assert.New(t)
+
+			obj := testCase.Input.Object
+			err := obj.Set(testCase.Input.BeSet, testCase.Input.Path, testCase.Input.Paths...)
 
 			assert.Equal(testCase.Expect.Object, obj)
 			assert.Equal(testCase.Expect.Err, err)
