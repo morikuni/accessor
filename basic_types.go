e2ec390b9b32fc64c6287a587955f3a81aed161e
morikuni <taihei.with+git@gmail.com>
2017-02-16 11:55:34 +0900

    Add BasicTypes

---
+++ basic_types.go
@@ -0,0 +1,34 @@
+package undef
+
+import (
+	"encoding"
+	"encoding/json"
+	"fmt"
+)
+
+type BasicTypes struct {
+	Value interface{}
+}
+
+func (o BasicTypes) Get(path string, paths ...string) (Object, error) {
+	return nil, NewPathError(fmt.Sprintf("%[1]T(%[1]v) has no key", o.Value), path)
+}
+
+func (o BasicTypes) Set(obj Object, path string, paths ...string) error {
+	return NewPathError(fmt.Sprintf("%[1]T(%[1]v) has no key", o.Value), path)
+}
+
+func (o BasicTypes) MarshalJSON() ([]byte, error) {
+	return json.Marshal(o.Value)
+}
+
+func (o BasicTypes) MarshalText() ([]byte, error) {
+	if m, ok := o.Value.(encoding.TextMarshaler); ok {
+		return m.MarshalText()
+	}
+	return []byte(fmt.Sprint(o.Value)), nil
+}
+
+func (o BasicTypes) MarshalYAML() (interface{}, error) {
+	return o.Value, nil
+}
